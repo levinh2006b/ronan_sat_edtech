eb28631957b824c7a069dc0a1c23858e30f63928
levinh2006b <ledinhvinh7a2@gmail.com>
2026-04-20 18:23:24 +0700

    fix: sorting

--- types/testManager.ts
+++ types/testManager.ts
@@ -1,5 +1,10 @@
+// Admin manage all the questions in database
+
+
+// Allow to find the keyword that admin typed to be found in these 3 places
 export type TestManagerCatalogSearchScope = "testTitle" | "passage" | "options";
 
+// List sort options
 export type TestManagerCatalogSortOption =
   | "updated_desc"
   | "updated_asc"
@@ -8,6 +13,8 @@ export type TestManagerCatalogSortOption =
   | "question_asc"
   | "question_desc";
 
+
+// Info of a specific question in a row
 export type TestManagerCatalogRow = {
   questionId: string;
   testId: string;
@@ -22,11 +29,12 @@ export type TestManagerCatalogRow = {
   updatedAt: string;
 };
 
+// Pagination
 export type TestManagerCatalogPage = {
-  rows: TestManagerCatalogRow[];
-  total: number;
-  offset: number;
-  limit: number;
-  nextOffset: number;
+  rows: TestManagerCatalogRow[];  // List questions in a row
+  total: number;                  
+  offset: number;                 // The starting point to get data ( in page 2, offset is 20 to skip 20 ques in first page )
+  limit: number;                  // num of ques in a page
+  nextOffset: number;             // contain the offset for the next page + bool to check if next page has any ques
   hasMore: boolean;
 };
